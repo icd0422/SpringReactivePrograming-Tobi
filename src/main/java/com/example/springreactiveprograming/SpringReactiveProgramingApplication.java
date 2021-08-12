c7b6b83d0c6c97ccb20be4489bf0179d4c96ffe6
장준희 <junheejang@wemakeprice.com>
2021-08-12 11:39:16 +0900

    Initial commit

---
+++ src/main/java/com/example/springreactiveprograming/SpringReactiveProgramingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactiveprograming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveProgramingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveProgramingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactiveprograming/SpringReactiveProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactiveprograming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveProgramingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
