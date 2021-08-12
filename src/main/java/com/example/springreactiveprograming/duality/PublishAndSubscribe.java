14987cd181789cac6c54b8607038a0baacd50ea2
장준희 <junheejang@wemakeprice.com>
2021-08-12 17:02:49 +0900

    feat: PublishAndSubscribe 비동기 처리

--- src/main/java/com/example/springreactiveprograming/duality/PublishAndSubscribe.java
+++ src/main/java/com/example/springreactiveprograming/duality/PublishAndSubscribe.java
@@ -7,35 +7,42 @@ import org.reactivestreams.Subscription;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class PublishAndSubscribe {
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
         Publisher publisher = new Publisher() {
 
             final Iterable<Integer> iterable = Arrays.asList(1, 2, 3, 4, 5);
 
             @Override
             public void subscribe(Subscriber subscriber) {
-                log.debug("subscribe");
                 subscriber.onSubscribe(new Subscription() {
 
                     Iterator iterator = iterable.iterator();
 
                     @Override
-                    public void request(long n) {
-                        try {
-                            while (n-- > 0) {
-                                if (iterator.hasNext()) {
-                                    subscriber.onNext(iterator.next());
-                                } else {
-                                    subscriber.onComplete();
-                                    break;
+                    public void request(final long n) {
+                        executorService.execute(() -> {
+                            long nn = n;
+                            try {
+                                while (nn-- > 0) {
+                                    if (iterator.hasNext()) {
+                                        subscriber.onNext(iterator.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
                                 }
+                            } catch (Exception e) {
+                                subscriber.onError(e);
                             }
-                        } catch (Exception e) {
-                            subscriber.onError(e);
-                        }
+                        });
                     }
 
                     @Override
@@ -56,7 +63,6 @@ public class PublishAndSubscribe {
             @Override
             public void onNext(Object o) {
                 log.debug("onNext : {}", o);
-                throw new RuntimeException();
             }
 
             @Override
@@ -70,6 +76,8 @@ public class PublishAndSubscribe {
             }
         };
 
+        log.debug("start");
         publisher.subscribe(subscriber);
+        log.debug("end");
     }
 }
