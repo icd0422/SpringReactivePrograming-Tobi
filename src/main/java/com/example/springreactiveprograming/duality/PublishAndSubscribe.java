de0cd745aa746c891d4ffb42fe8947571ef02482
장준희 <junheejang@wemakeprice.com>
2021-08-12 16:04:55 +0900

    feat: PublishAndSubscribe 추가

---
+++ src/main/java/com/example/springreactiveprograming/duality/PublishAndSubscribe.java
@@ -0,0 +1,70 @@
+package com.example.springreactiveprograming.duality;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+@Slf4j
+public class PublishAndSubscribe {
+    public static void main(String[] args) {
+        Publisher publisher = new Publisher() {
+
+            final Iterable<Integer> iterable = Arrays.asList(1, 2, 3, 4, 5);
+
+            @Override
+            public void subscribe(Subscriber subscriber) {
+                log.debug("subscribe");
+                subscriber.onSubscribe(new Subscription() {
+
+                    Iterator iterator = iterable.iterator();
+
+                    @Override
+                    public void request(long n) {
+                        while (n-- > 0) {
+                            if (iterator.hasNext()) {
+                                subscriber.onNext(iterator.next());
+                            } else {
+                                subscriber.onComplete();
+                                break;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber subscriber = new Subscriber() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Object o) {
+                log.debug("onNext : {}", o);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        };
+
+        publisher.subscribe(subscriber);
+    }
+}
